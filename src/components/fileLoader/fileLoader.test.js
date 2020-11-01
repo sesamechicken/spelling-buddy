42aeddae3a6caee0b65101c28ceb4ea2b28e4aa9
Chris Dickow <chrisdickow@gmail.com>
2020-11-01 10:27:26 -0500

    Cleaned up the fileLoader test as it was making everything take too long. Don't need a new instance of the component for each test since we're just asserting against it's output

--- src/components/fileLoader/fileLoader.test.js
+++ src/components/fileLoader/fileLoader.test.js
@@ -1,23 +1,19 @@
 import React from "react";
-import { shallow } from "enzyme";
 import { Input, Button } from "semantic-ui-react";
 
 import { FileLoader } from "./index";
 
 describe("renders an expected Fragment", () => {
-    /** The "shallow" container for Enzyme to use for our React DOM "parent" */
-    let container;
 
-    /** Render our FileLoader Component for inspection. */
-    beforeAll(() => { container = shallow(<FileLoader />); } );
+  const wrapper = shallow(<FileLoader />);
 
     test("Fragment has a load button", () => {
-        expect( container.containsMatchingElement(
+        expect( wrapper.containsMatchingElement(
             <Button>Load words</Button>
         )).toBe(true);
     });
     test("Fragment has an input for the file / filename", () => {
-        expect( container.containsMatchingElement(
+        expect( wrapper.containsMatchingElement(
             <Input />
         )).toBe(true);
     });
