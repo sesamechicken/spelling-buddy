3189b1244f6141660eb46815d76fd10e6fc74921
chris <chrisdickow@gmail.com>
2020-11-03 19:12:45 -0500

    Updated readme and fileLoader test description

--- src/components/fileLoader/fileLoader.test.js
+++ src/components/fileLoader/fileLoader.test.js
@@ -3,16 +3,16 @@ import { Input, Button } from "semantic-ui-react";
 
 import { FileLoader } from "./index";
 
-describe("renders an expected Fragment", () => {
+describe("Fileloader", () => {
 
   const wrapper = shallow(<FileLoader />);
 
-    test("Fragment has a load button", () => {
+    test("has a load button", () => {
         expect( wrapper.containsMatchingElement(
             <Button>Load words</Button>
         )).toBe(true);
     });
-    test("Fragment has an input for the file / filename", () => {
+    test("has an input for the file / filename", () => {
         expect( wrapper.containsMatchingElement(
             <Input />
         )).toBe(true);
